4670bc26d490f0d9f29ae9d3013b19d64ea7fe9f
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-09-05 12:21:40 +0400

    задача68

---
+++ задача68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа найдет сумму натуральных элементов от М до N
+Console.WriteLine("Введите начальное число M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int N = int.Parse(Console.ReadLine());
+
+void Sum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+Sum(M, N, sum);
+}
+
+Sum(M, N, 0);
