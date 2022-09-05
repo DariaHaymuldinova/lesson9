2f86ca9d191125bccfe8287da408688255fdd0c4
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-09-05 10:50:36 +0400

    задача64

---
+++ задача64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа выведет все натуральные числа в промежутке от N до M
+int Foo (int N, int M)
+{
+    if (N < M)
+    {
+        return M;
+    }
+    Console.Write("{0}", M + " ");
+    M++;
+    return Foo (N, M);
+}
+Foo(20, 10);
