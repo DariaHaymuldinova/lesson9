1729babee9c80f52021799f5f05e5ff08878af69
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-09-05 11:10:58 +0400

    задача66

---
+++ задача66/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа вычисляет функцию Аккермана;
+int Accerman(int N, int M)
+{
+    if (N == 0)
+        return M + 1;
+    else
+    if ((N != 0) && (M == 0))
+        return Accerman(N - 1, 1);
+    else
+        return Accerman(N - 1, Accerman(N, M - 1));
+}
+Console.WriteLine(Accerman(2,2));
